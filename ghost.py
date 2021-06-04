db91dfeed0b1ba387d391647381be499976aee3a
Feng Zhao <fengzhao@gatech.edu>
2021-06-03 19:19:06 -0500

    Add method docstrings for the Ghost class

--- ghost.py
+++ ghost.py
@@ -1,8 +1,8 @@
 import random
 
+
 # Implement a Trie data structure
 class Trie:
-
 
     def __init__(self):
         self.dict = {}
@@ -84,19 +84,20 @@ class Player:
             print("\nThat's not a letter")
 
 
+# Implement a Ghost class for the game
 class Ghost:
 
-    lookup = None
-
-    def __init__(self, word_trie):
-        self.player_list = []
+    def __init__(self, player_list, word_trie):
+        self.player_list = player_list
         self.current_player = None
         self.word_trie = word_trie
 
     def play(self) -> None:
+        """
+        Main method in the class.
+        """
         prefix = ""
 
-        self.initialize_players()
         self.starts_first()
 
         while True:
@@ -110,7 +111,7 @@ class Ghost:
                 if not self.valid_move(prefix):
                     strike_count += 1
                     self.current_player.set_strike_count(strike_count)
-                    print("Sorry, that's not a prefix of a real word. Try again!\n")
+                    print("\nSorry, the letter you entered does not form the beginning of a valid word. Try again!\n")
                     print(f"You've got {strike_count} strike(s)!")
                     if strike_count < 3:
                         self.current_player.enter_letter()
@@ -125,42 +126,33 @@ class Ghost:
         print(f"\n{self.current_player.name}, you lost!")
         print("Game over!")
 
-    def initialize_players(self) -> None:
-        print("Welcome to the Ghost game!\n")
-        name1 = input("Player1, please enter your name when you are ready.\n")
-        player1 = Player(name1)
-        self.player_list.append(player1)
-
-        name2 = input("\nPlayer2, please enter your name when you are ready.\n")
-
-        while not self.valid_name(name1, name2):
-            name2 = input(
-                "\nPlayer2, the name you entered has been taken. Please enter a different name when you are ready.\n")
-
-        player2 = Player(name2)
-        self.player_list.append(player2)
-
     def starts_first(self) -> None:
+        """
+        Randomly assign a player to enter first.
+        """
         self.current_player = random.choice(self.player_list)
 
     def change_turns(self) -> None:
+        """
+        Take turns for the players.
+        """
         if self.current_player == self.player_list[0]:
             self.current_player = self.player_list[1]
         else:
             self.current_player = self.player_list[0]
 
-    @staticmethod
-    def valid_name(name1, name2) -> bool:
-        if name2 != name1:
-            return True
-        return False
-
     def valid_move(self, prefix) -> bool:
+        """
+        Returns if the letter entered will form the beginning of a valid word.
+        """
         if self.word_trie.is_prefix(prefix):
             return True
         return False
 
     def game_over(self, player, prefix) -> bool:
+        """
+        Returns if the game is over.
+        """
         if player.get_strike_count() == 3:
             return True
         elif len(prefix) > 3 and self.word_trie.is_word(prefix):
