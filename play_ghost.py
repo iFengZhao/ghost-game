5c515d6c04db71136b26729f8afa88eb3d3a46b3
Feng Zhao <fengzhao@gatech.edu>
2021-06-03 20:06:18 -0500

    Implement a separate python file to start the ghost game.

---
+++ play_ghost.py
@@ -0,0 +1,33 @@
+
+
+if __name__ == '__main__':
+
+    import random
+    from ghost import Trie, Player, Ghost
+
+    # Create Trie object with data from text file
+    with open('wordlist_kevin_atkinson.txt') as f:
+        word_list = f.read().splitlines()
+
+    word_trie = Trie()
+    for word in word_list:
+        word_trie.insert(word)
+
+    player_list = []
+
+    print("Welcome to the Ghost game!\n"
+          "Make sure you understand the rules before playing.\n")
+
+    name1 = input("Player1, please enter your name when you are ready.\n")
+    player1 = Player(name1)
+    player_list.append(player1)
+
+    name2 = input("\nPlayer2, please enter your name when you are ready.\n")
+    while name2 == name1:
+        print("\nSorry, the name you entered has been taken.\n")
+        name2 = input("\nPlayer2, please enter your name when you are ready.\n")
+    player2 = Player(name2)
+    player_list.append(player2)
+
+    game = Ghost(player_list, word_trie)
+    game.play()
